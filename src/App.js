9be6c340df91f6b847dfa8f6873247dd63df03f1
mateusabe <mateus.miranda@outlook.com>
2025-07-07 08:25:18 -0300

    Creating respository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+function App(){
+  const [input,setInput] = useState('');
+  const [tarefas,setTarefas] = useState([
+    'Estuadar React',
+    'Estudar Node',
+  ]);
 
-function App() {
+  function handleTasks(e){
+    e.preventDefault();
+
+    setTarefas([...tarefas,input]);
+    setInput('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <form onSubmit={(e) => handleTasks(e)}>
+        <label>Nome da tarefa:</label><br/>
+        <input value={input} onChange={(e) => setInput(e.target.value)}/><br/>
+
+        <button type="submit" on>Cadastrar</button><br/>
+      </form>
+
+      <ul>
+        {tarefas.map((tarefa) => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+    </div>    
   );
 }
 
 export default App;
+
---
+++ src/Nome.js
@@ -0,0 +1,11 @@
+function Nome(props){
+  return (
+    <div>
+      <h3>Welcome, youngy</h3>
+      <p>Name: {props.aluno}</p>
+      <p>Age: {props.idade}</p>
+    </div>   
+  );
+}
+
+export default Nome;
