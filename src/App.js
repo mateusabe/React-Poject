dfa6b8143493b8d7e5e568290ebbb16c98c19997
mateusabe <mateus.miranda@outlook.com>
2025-07-07 09:16:27 -0300

    feat, show data from fetch in html

--- src/App.js
+++ src/App.js
@@ -1,50 +1,69 @@
-import { useState, useEffect, use } from 'react';
+import { useState, useEffect } from 'react'
+
+import './style.css'
 
 function App() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState('')
+
+  const [nutri, setNutri] = useState([]);
 
   const [tarefas, setTarefas] = useState(() => {
-    const tarefasStorage = localStorage.getItem('@tarefas');
-    return tarefasStorage ? JSON.parse(tarefasStorage) : ['Estudar React', 'Estudar Node'];
-  });
+    const tarefasStorage = localStorage.getItem('@tarefas')
+    return tarefasStorage
+      ? JSON.parse(tarefasStorage)
+      : ['Estudar React', 'Estudar Node']
+  })
 
   useEffect(() => {
-    localStorage.setItem('@tarefas', JSON.stringify(tarefas));    
-  }, [tarefas]);
+    localStorage.setItem('@tarefas', JSON.stringify(tarefas))
+  }, [tarefas])
 
-  useEffect(() =>{
-    function getApi(){
+  useEffect(() => {
+    function getApi() {
       fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => console.log(data));
+      .then((response) => response.json())
+      .then((data) => {
+          setNutri(data)
+        }
+      )
     }
 
-    getApi();
-  },[])
+    getApi()
+  }, [])
 
   function handleTasks(e) {
-    e.preventDefault();
-    if (input.trim() === '') return;
+    e.preventDefault()
+    if (input.trim() === '') return
 
-    setTarefas([...tarefas, input.trim()]);
-    setInput('');
+    setTarefas([...tarefas, input.trim()])
+    setInput('')
   }
 
   return (
-    <div>
+    <div className="container">
       <form onSubmit={handleTasks}>
-        <label>Nome da tarefa:</label><br />
-        <input value={input} onChange={(e) => setInput(e.target.value)} /><br />
-        <button type="submit">Cadastrar</button><br />
+        <label>Nome da tarefa:</label>
+        <br />
+        <input value={input} onChange={e => setInput(e.target.value)} />
+        <br />
+        <button type="submit">Cadastrar</button>
+        <br />
+        <div>
+          {nutri.map((item) => {
+            return(
+              <article key={item.id}>
+                <strong>{item.name}</strong>
+                <strong> {item.username}</strong>
+                <strong>, {item.phone}</strong>
+              </article>
+            )
+          })}
+        </div>
       </form>
 
-      <ul>
-        {tarefas.map((tarefa, index) => (
-          <li key={index}>{tarefa}</li>
-        ))}
-      </ul>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
